3ef0f5e494a3b362df8afa7268cc609d98df5ae7
Ajai Rajendran <Ajai.Rajendran@ibm.com>
2025-01-17 16:11:51 +0530

    submit functionality added

--- todo_app/src/App.js
+++ todo_app/src/App.js
@@ -1,21 +1,22 @@
-import './App.css';
-
+import {React, useId, useState} from "react";
+import Form from "./components/Form";
+import Header from "./components/Header";
+import ToDoHero from "./components/ToDoHero";
+import ToDoList from "./components/ToDoList";
 function App() {
+  const [toDos, setToDos] = useState([]);
+  
+  const todos_completed = toDos.filter(toDo=>toDo.isCompleted === true).length;
+  const total_todos = toDos.length; 
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <Header />
+      <div className="app-container">
+        <ToDoHero todos_completed={todos_completed} total_todos={total_todos} />
+        <Form setToDos={setToDos}/>
+      </div>
+      
+      <ToDoList todos={toDos} />
     </div>
   );
 }
--- todo_app/src/components/ToDoHero.jsx
+++ todo_app/src/components/ToDoHero.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 function ToDoHero({ todos_completed, total_todos }) {
     return (
-      <section>
-        <div>
-          <p>Task Done</p>
-          <p>Keep it up</p>
+        <section className="todohero_section">
+        <div className='p10'>
+          <p className="text_large">Task Done</p>
+          <p className="text_small">Keep it up</p>
         </div>
         <div>
           {todos_completed}/{total_todos}
